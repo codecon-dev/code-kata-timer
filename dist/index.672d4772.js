4c595b1c01c80644bcf58ee15a9f8e130ffa06dd
Gabriel Nunes <gnunesmail@gmail.com>
2025-03-17 11:32:51 -0300

    first commit

---
+++ dist/index.672d4772.js
@@ -0,0 +1,69 @@
+let intervalInMillis = 10;
+let timerStarted = false;
+let millis = 0;
+let seconds = 30;
+let minutes = 0;
+let hours = 0;
+let intervalInstance;
+document.getElementById("hour").value = hours;
+document.getElementById("min").value = minutes;
+document.getElementById("sec").value = seconds;
+document.getElementById("startBtn").addEventListener("click", function() {
+    timerStarted = true;
+    run();
+});
+document.getElementById("pauseBtn").addEventListener("click", function() {
+    console.log("pause");
+    pause();
+});
+document.getElementById("toZeroBtn").addEventListener("click", function() {
+    pause();
+    hours = 0;
+    minutes = 0;
+    seconds = 30;
+    document.getElementById("hour").value = hours;
+    document.getElementById("min").value = minutes;
+    document.getElementById("sec").value = seconds;
+});
+function run() {
+    hours = document.getElementById("hour").value;
+    minutes = document.getElementById("min").value;
+    seconds = document.getElementById("sec").value;
+    intervalInstance = setInterval(function() {
+        if (timerStarted) {
+            seconds--;
+            if (seconds == 0) {
+                if (minutes > 0) {
+                    minutes--;
+                    seconds = 0;
+                    seconds = 59;
+                } else {
+                    if (hours > 0) ;
+                    else {
+                        timerStarted = false;
+                        clearInterval(intervalInstance);
+                    }
+                }
+            }
+            if (minutes == 0) {
+                if (hours > 0) {
+                    hours--;
+                    minutes = 59;
+                }
+            // sem condição
+            }
+            document.getElementById(`sec`).value = seconds;
+            document.getElementById(`min`).value = minutes;
+            document.getElementById(`hour`).value = hours;
+        }
+    }, 1000);
+}
+function pause() {
+    timerStarted = false;
+    clearInterval(intervalInstance);
+}
+function stop() {
+    timerStarted = false;
+} // functio
+
+//# sourceMappingURL=index.672d4772.js.map
