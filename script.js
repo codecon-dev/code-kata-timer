e8002ed0e31194ee3221a5330b71cdea764b8204
PethersonML <petherson.monteiro@hotmail.com>
2025-03-17 23:27:49 -0300

    - Adiciona funções de `Edição; - Refatora boa parte do código feito até então;

--- script.js
+++ script.js
@@ -1,11 +1,112 @@
 let timerStarted = false;
 let seconds = 0;
+let sec = 0;
+let min = 0;
+let hour = 0;
 let intervalInstance;
 
+const timeInputs = document.querySelector('.input-stopwatch');
+
+const startBtn = document.getElementById("startBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const editBtn = document.getElementById("editBtn");
+const cancelEditBtn = document.getElementById("cancelEditBtn");
+const confirmEditBtn = document.getElementById("confirmEditBtn");
+
+startBtn.addEventListener("click", function () {
+  if( !timerStarted ){
+    timerStarted = true;
+    run();
+  }
+});
+
+pauseBtn.addEventListener("click", function () {
+  pause();
+});
+
+resetBtn.addEventListener("click", function () {
+  reset();
+});
+
+editBtn.addEventListener("click", function () {
+  document.querySelector('.js-stopwatch-button').classList.add('hide');
+  document.querySelector('.js-edit-container-stopwatch').classList.remove('hide');
+
+  timeInputs.querySelectorAll('input').forEach(input => input.disabled = false);
+
+  hour = Number(document.getElementById("hour").value);
+  min = Number(document.getElementById("min").value);
+  sec = Number(document.getElementById("sec").value);
+});
+
+document.querySelectorAll('.input-stopwatch input').forEach(input => {
+  input.addEventListener('input', function () {
+    let max = input.id === 'hour' ? 99 : 59;
+    let min = 0;
+
+    const value = Number(input.value);
+
+    if( value >= 0 && value < 10 ){
+      input.value = '0' + value;
+    } else {
+      input.value = value;
+    }
+
+    if( value < min )
+      input.value = '00';
+
+    if( value > max )
+      input.value = max;
+    
+  });
+});
+
+cancelEditBtn.addEventListener("click", function () {
+  document.querySelector('.js-stopwatch-button').classList.remove('hide');
+  document.querySelector('.js-edit-container-stopwatch').classList.add('hide');
+
+  timeInputs.querySelectorAll('input').forEach(input => input.disabled = true);
+
+  document.getElementById("hour").value = hour >= 0 && hour < 10 ? '0' + hour : hour;
+  document.getElementById("min").value = min >= 0 && min < 10  ? '0' + min : min;
+  document.getElementById("sec").value = sec >= 0 && sec < 10 ? '0' + sec : sec;
+});
+
+confirmEditBtn.addEventListener("click", function () {
+  document.querySelector('.js-stopwatch-button').classList.remove('hide');
+  document.querySelector('.js-edit-container-stopwatch').classList.add('hide');
+
+  timeInputs.querySelectorAll('input').forEach(input => input.disabled = true);
+});
+
+function run() {
+  if( timerStarted ) {
+    startBtn.classList.add('hide');
+    pauseBtn.classList.remove('hide');
+    editBtn.disabled = true;
+
+    intervalInstance = setInterval(function () {
+      convertInputsToSeconds();
+      seconds--;
+
+      if( seconds >= 0 ){
+        convertSecondsToInputs();
+      } else {
+        timerStarted = false;
+        clearInterval(intervalInstance);
+        startBtn.classList.remove('hide');
+        pauseBtn.classList.add('hide');
+        editBtn.disabled = false;
+      }
+    }, 1000);
+  }
+}
+
 const convertInputsToSeconds = () => {
-  const hour = Number(document.getElementById("hour").value);
-  const min = Number(document.getElementById("min").value);
-  const sec = Number(document.getElementById("sec").value);
+  hour = Number(document.getElementById("hour").value);
+  min = Number(document.getElementById("min").value);
+  sec = Number(document.getElementById("sec").value);
 
   seconds = sec;
 
@@ -17,9 +118,9 @@ const convertInputsToSeconds = () => {
 }
 
 const convertSecondsToInputs = () => {
-  let hour = 0;
-  let min = 0;
-  let sec = 0;
+  hour = 0;
+  min = 0;
+  sec = 0;
 
   if( seconds > 3600 ){
     hour = Math.floor(  seconds / 3600  );
@@ -33,53 +134,26 @@ const convertSecondsToInputs = () => {
     sec = seconds;
   }
 
-  document.getElementById("hour").value = hour < 10 && hour > 0 ? '0' + hour : hour == 0 ? '00' : hour;
-  document.getElementById("min").value = min < 10 && min > 0 ? '0' + min : min == 0 ? '00' : min;
-  document.getElementById("sec").value = sec < 10 && sec > 0 ? '0' + sec : sec == 0 ? '00' : sec;
-}
-
-document.getElementById("startBtn").addEventListener("click", function () {
-  timerStarted = true;
-  run();
-});
-
-document.getElementById("pauseBtn").addEventListener("click", function () {
-  pause();
-});
-
-document.getElementById("toZeroBtn").addEventListener("click", function () {
-  pause();
-  hours = 0;
-  minutes = 0;
-  seconds = 30;
-
-  document.getElementById("hour").value = hours;
-  document.getElementById("min").value = minutes;
-  document.getElementById("sec").value = seconds;
-});
-
-function run() {
-  intervalInstance = setInterval(function () {
-    if (timerStarted) {
-      convertInputsToSeconds();
-      seconds--;
-
-      if( seconds >= 0 ){
-        convertSecondsToInputs();
-      } else {
-        timerStarted = false;
-        clearInterval(intervalInstance);
-      }
-    }
-  }, 1000);
+  document.getElementById("hour").value = hour >= 0 && hour < 10 ? '0' + hour : hour;
+  document.getElementById("min").value = min >= 0 && min < 10  ? '0' + min : min;
+  document.getElementById("sec").value = sec >= 0 && sec < 10 ? '0' + sec : sec;
 }
 
 function pause() {
   timerStarted = false;
   clearInterval(intervalInstance);
+  startBtn.classList.remove('hide');
+  pauseBtn.classList.add('hide');
+  editBtn.disabled = false;
 }
 
-function stop() {
+function reset() {
   timerStarted = false;
+  clearInterval(intervalInstance);
+  document.getElementById("hour").value = '00';
+  document.getElementById("min").value = '00';
+  document.getElementById("sec").value = 30;
+  startBtn.classList.remove('hide');
+  pauseBtn.classList.add('hide');
+  editBtn.disabled = false;
 }
-// functio
