e2696405c185b26d50456d3af25cfe4e716002b5
hildeir <hildeirdaniel@gmail.com>
2025-03-18 18:06:07 -0300

    ajustei a funcionalidade edit e os icon; adicionei a funcionalidade dos 10 seconds e alteração do fundo nos 0 seconds

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let minutes = 0;
 let hours = 0;
 let intervalInstance = null;
 
-document.getElementById("hour").value = hours;
-document.getElementById("min").value = minutes;
-document.getElementById("sec").value = seconds;
+document.getElementById("hour").innerText = hours;
+document.getElementById("min").innerText = minutes;
+document.getElementById("sec").innerText = seconds;
 
 document.getElementById("startBtn").addEventListener("click", function () {
   if (!timerStarted) {
@@ -36,14 +36,15 @@ document.getElementById("toZeroBtn").addEventListener("click", function () {
 });
 
 function run() {
-  hours = Math.max(0, parseInt(document.getElementById("hour").value, 10) || 0);
+  
+  hours = Math.max(0, parseInt(document.getElementById("hour").innerText, 10) || 0);
   minutes = Math.max(
     0,
-    parseInt(document.getElementById("min").value, 10) || 0
+    parseInt(document.getElementById("min").innerText, 10) || 0
   );
   seconds = Math.max(
     0,
-    parseInt(document.getElementById("sec").value, 10) || 0
+    parseInt(document.getElementById("sec").innerText, 10) || 0
   );
 
   if (intervalInstance) {
@@ -66,18 +67,45 @@ function run() {
         timerStarted = false;
         clearInterval(intervalInstance);
         intervalInstance = null;
+        lastStep();
         return;
       }
     } else {
-      seconds--;
+
+      if(seconds === 10){
+        tenSeconds();
+      }
+      seconds--; 
     }
 
-    document.getElementById("sec").value = seconds;
-    document.getElementById("min").value = minutes;
-    document.getElementById("hour").value = hours;
+    document.getElementById("sec").innerText = seconds;
+    document.getElementById("min").innerText = minutes;
+    document.getElementById("hour").innerText = hours;
   }, DEFAULT_INTERVAL);
 }
+/**** última etapa, fim */
+function lastStep(){
+  const body = document.getElementById("body");
+  body.classList.remove("body-background-and-colorFont");
+  body.classList.add("fim-step");
 
+  const buttonWrapper = document.querySelector(".button-wrapper");
+  buttonWrapper.style.display = "none";
+}
+/**** fim  ****/
+/***** quando chegar nos 10 segundos ****/
+function tenSeconds(){
+  const btHour = document.getElementById("hour");
+  const btMin = document.getElementById("min");
+  const separeteTime = document.querySelectorAll(".separete-time");
+  separeteTime.forEach((element) =>{
+    element.classList.add("hidden-hour-minutes-separete");
+  })
+  
+  btHour.classList.add("hidden-hour-minutes-separete");
+  btMin.classList.add("hidden-hour-minutes-separete");
+}
+/**** fim  ****/
 function pause() {
   timerStarted = false;
   if (intervalInstance) {
@@ -96,6 +124,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const editButton = document.querySelector(".js-edit-stopwatch");
   const editContainer = document.querySelector(".js-edit-container-stopwatch");
   const stopwatchButtons = document.querySelector(".js-stopwatch-button");
+  const btHour = document.getElementById("hour");
+  const btMin = document.getElementById("min");
+  const btSeconds = document.getElementById("sec");
+  let saveHour;
+  let saveMin;
+  let saveSeconds;
 
   fullscreenButton.addEventListener("click", () => {
     if (!document.fullscreenElement) {
@@ -106,27 +140,55 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   });
-
+  /**** ativa e desativa edição */
+  function enableEdit(){
+    btHour.setAttribute("contenteditable",true);
+    btMin.setAttribute("contenteditable",true);
+    btSeconds.setAttribute("contenteditable",true);
+  }
+  function disableEdit(){
+    btHour.removeAttribute("contenteditable");
+    btMin.removeAttribute("contenteditable");
+    btSeconds.removeAttribute("contenteditable");
+  }
+  /*** fim */
+  /*** funcionalidade: volta ao time anterior ***/
+  function backTimeBeforeEdit(){
+    btHour.innerText = saveHour;
+    btMin.innerText = saveMin;
+    btSeconds.innerText = saveSeconds;
+  }
+  function saveTimeBeforeEdit(){
+    saveHour = btHour.innerText;
+    saveMin = btMin.innerText;
+    saveSeconds = btSeconds.innerText;
+  }
+  /**** fim *****/
   editButton.addEventListener("click", () => {
+    enableEdit();
+    saveTimeBeforeEdit();
     editContainer.classList.toggle("hide");
     stopwatchButtons.classList.toggle("hide");
   });
 
   const cancelButton = document.querySelector(".js-cancel-button");
   cancelButton.addEventListener("click", () => {
+    disableEdit();
+    backTimeBeforeEdit();
     editContainer.classList.add("hide");
     stopwatchButtons.classList.remove("hide");
   });
 
   const finishEditButton = document.querySelector(".js-finish-edit-button");
   finishEditButton.addEventListener("click", () => {
+    disableEdit();
     editContainer.classList.add("hide");
     stopwatchButtons.classList.remove("hide");
-    hours = parseInt(document.getElementById("hour").value, 10);
-    minutes = parseInt(document.getElementById("min").value, 10);
-    seconds = parseInt(document.getElementById("sec").value, 10);
-    document.getElementById("hour").value = hours;
-    document.getElementById("min").value = minutes;
-    document.getElementById("sec").value = seconds;
+    hours = parseInt(document.getElementById("hour").innerText, 10);
+    minutes = parseInt(document.getElementById("min").innerText, 10);
+    seconds = parseInt(document.getElementById("sec").innerText, 10);
+    document.getElementById("hour").innerText = hours;
+    document.getElementById("min").innerText = minutes;
+    document.getElementById("sec").innerText = seconds;
   });
 });
