693a17453f9a3ed668af7553eddbe5a6e46c3c35
Gean Farias <gean.farias@outlook.com>
2025-04-11 17:47:03 -0300

    criando arquivo de formatação do prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    printWidth: 100,
+    endOfLine: 'lf',
+    quoteProps: 'as-needed',
+};
