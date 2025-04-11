8f7384c452362039af4f721d4624f7f5dda44baa
Gean Farias <gean.farias@outlook.com>
2025-04-11 17:47:26 -0300

    atualizando helper

--- asset/javascript/helper.js
+++ asset/javascript/helper.js
@@ -1,11 +1,11 @@
 HTMLElement.prototype.showElement = function showElement() {
-    if (this.classList.contains("hide")) {
-        this.classList.remove("hide");
+    if (this.classList.contains('hide')) {
+        this.classList.remove('hide');
     }
 };
 
 HTMLElement.prototype.hideElement = function hideElement() {
-    if (!this.classList.contains("hide")) {
-        this.classList.add("hide");
+    if (!this.classList.contains('hide')) {
+        this.classList.add('hide');
     }
 };
